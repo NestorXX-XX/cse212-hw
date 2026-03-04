ec26a82b89aa9b26f4597a6c72bdc05cd6200413
Nestor Gomez <nestorgomezartiles@gmail.com>
2026-03-04 22:51:30 +0100

    W01 Code: Dynamic Arrays

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -1,3 +1,5 @@
+using System.IO.Pipelines;
+
 public static class ArraySelector
 {
     public static void Run()
@@ -11,6 +13,22 @@ public static class ArraySelector
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        int countList1 = 0;
+        int countList2 = 0;
+        int[] result = new int[select.Length];
+        for (int i = 0; i < select.Length; i++)
+        {
+            if (select[i] == 1)
+            {
+                result[i] = list1[countList1];
+                countList1 += 1;
+            }
+            else
+            {
+                result[i] = list2[countList2];
+                countList2 += 1;
+            }
+        }
+        return result;
     }
 }
